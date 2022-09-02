9dcd4516ffb230a6577b39c4a7188a09402347ce
DaeHyeonHong <fholng@outlook.com>
2022-09-02 19:04:38 +0900

    PK allocationSize 속성

--- src/main/java/com/example/ex1hellojpa/JpaMain.java
+++ src/main/java/com/example/ex1hellojpa/JpaMain.java
@@ -1,11 +1,13 @@
 package com.example.ex1hellojpa;
 
 import com.example.ex1hellojpa.member.Member;
+import lombok.extern.slf4j.Slf4j;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+@Slf4j
 public class JpaMain {
     public static void main(String[] args) {
         final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
@@ -14,9 +16,23 @@ public class JpaMain {
         transaction.begin();
 
         try {
-            final Member member = new Member();
-            member.setName("감자");
-            entityManager.persist(member);
+            final Member member1 = new Member();
+            member1.setName("감자");
+            final Member member2 = new Member();
+            member2.setName("고구마");
+            final Member member3 = new Member();
+            member3.setName("김치");
+            log.info("======================");
+            log.info("member1 : {}", member1);
+            log.info("member2 : {}", member2);
+            log.info("member3 : {}", member3);
+            entityManager.persist(member1);
+            entityManager.persist(member2);
+            entityManager.persist(member3);
+            log.info("======================");
+            log.info("member1 : {}", member1);
+            log.info("member2 : {}", member2);
+            log.info("member3 : {}", member3);
             transaction.commit();
         } catch (final Exception exception) {
             transaction.rollback();
--- src/main/java/com/example/ex1hellojpa/member/Member.java
+++ src/main/java/com/example/ex1hellojpa/member/Member.java
@@ -7,21 +7,18 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.TableGenerator;
-import static javax.persistence.GenerationType.TABLE;
+import javax.persistence.SequenceGenerator;
+import static javax.persistence.GenerationType.SEQUENCE;
 import static lombok.AccessLevel.PRIVATE;
 
 @Entity
 @Setter
 @ToString
 @FieldDefaults(level = PRIVATE)
-@TableGenerator(
-        name = "MEMBER_SEQUENCE_GENERATOR",
-        table = "MEMBER_SEQUENCE",
-        allocationSize = 1)
+@SequenceGenerator(name = "MEMBER_SEQUENCE_GENERATOR", sequenceName = "MEMBER_SEQUENCE")
 public class Member {
     @Id
-    @GeneratedValue(strategy = TABLE, generator = "MEMBER_SEQUENCE_GENERATOR")
+    @GeneratedValue(strategy = SEQUENCE, generator = "MEMBER_SEQUENCE_GENERATOR")
     Long id;
     @Column(unique = true, length = 10, updatable = false)
     String name;
